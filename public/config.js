bb2508f64d69517f41b8e0831e4014f9e2c62b0f
tuanhao1111 <tuanhao1111@gmail.com>
2026-05-17 20:56:56 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@
 // Thay URL này bằng Web App URL của Google Apps Script sau khi deploy
 // Hướng dẫn chi tiết xem trong file HUONG_DAN_DEPLOY.md
 const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyJRhOnMIiTysa4FlAwAG6gy519rpM-wOf4E9VC35Vu2__JS7VsWCVIdaIKte_uS3M/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzfLP8fcaBC5eavXY0GCglmPXyOr2_kPwsT1urL75XXzPyRRLWu3gmR93Pvzp5aG-M/exec',
 
   // Có cho phép mọi người xem danh sách công khai không? (true/false)
   SHOW_PUBLIC_LIST: true
